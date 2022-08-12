068031b65ee76fbb818b100dd57f997353942dba
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2022-08-12 02:20:08 -0400

    Simple UI and basic API response

--- front-end/src/pages/SearchPage.tsx
+++ front-end/src/pages/SearchPage.tsx
@@ -1,9 +1,56 @@
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+    width: 500px;
+    margin-bottom: 50px;
+
+    @media (max-width: 750px) {
+        width: 350px;
+    }
+`
+
+const MainContainer = styled.div`
+    display: flex;
+    margin-top: 200px;
+    flex-direction: column;
+    align-items: center;
+`
+
+
+const SearchArea = styled.div`
+    display: flex;
+    flex-direction: row;
+    max-width: 500px;
+    width: 100%;
+`
+
+const SearchInput = styled.input`
+    flex: 5;
+    margin-right: 20px;
+    font-size: 1.2rem;
+    font-family: 'Roboto', sans-serif;
+`
+
+const SearchButton = styled.button`
+    flex: 1;
+    transition: all 0.2s ease-in-out;
+`
+
 const SearchPage = () => {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>Search</h1>
-        </div>
+        <MainContainer>
+            <Logo src={require("../assets/logo.png")} />
+            <SearchArea>
+                <SearchInput onChange={e => setSearch(e.target.value)} />
+                <SearchButton onClick={() => {navigate(`/search?term=${search}`)}} disabled={search.length === 0}>Search</SearchButton>
+            </SearchArea>
+        </MainContainer>
     )
 }
 
--- front-end/src/pages/SearchResultsPage.tsx
+++ front-end/src/pages/SearchResultsPage.tsx
@@ -1,8 +1,39 @@
 
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
 const SearchResultsPage = () => {
+    const search = useLocation().search;
+    const name = new URLSearchParams(search).get("term");
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        // perform search on the API to get results
+        axios.get(`http://localhost:4000/api/search?term=${name}`).then(res => {
+            console.log(res.data);
+            setResults(res.data);
+        });
+    }, []);
+
     return (
         <div>
             <h1>Search Results</h1>
+            <p>
+                Here are all the results for <strong>{name}</strong>
+            </p>
+            <ul>
+                {results.map((result: any) => (
+                    <li key={result.id}>
+                        <a href={result.url}>{result.title}</a>
+                    </li>
+                ))}
+            </ul>
+            {
+                results.length === 0 && (
+                    <p>No results found</p>
+                )
+            }
         </div>
     )
 }
